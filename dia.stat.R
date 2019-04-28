d3e45e61ff16bf82ebf767cf4515549bcb779a7c
Yang Du <dyang627@qq.com>
2019-04-28 19:55:39 +0800

    Update dia.stat.R

--- dia.stat.R
+++ dia.stat.R
@@ -20,3 +20,54 @@ study.stat.tumor<-function(study.design.file){
 	print(paste0('No.tumorWithBioRep: ',No.tumorWithBioRep))
 
 }
+
+
+# length可以不相同，但如果不通，length小的数据，names 完全属于另一个;
+# levels必须相同; 
+compareTwoClusterClass<-function(c1,c2){
+	
+	if(!is.vector(c1) | !is.vector(c2)){
+		stop('input clusters must be vectors:err1')
+	}
+
+	cshort<-vector()
+	clarge<-vector()
+
+	if(length(c1)<=length(c2)){
+		cshort<-c1
+		clarge<-c2
+	}else{
+		cshort<-c2
+		clarge<-c1
+	}
+
+
+	if(!all(names(cshort) %in% names(clarge))){
+		stop('names of c1 and c2 not match:err2')
+	}
+
+	clarge <- clarge[names(cshort)]
+
+	require('e1071')
+
+	k=length(unique(cshort))
+
+	res<-vector()
+	perms<-permutations(k)
+
+	for(p in 1:nrow(perms)){
+
+		temp <- perms[p,]
+		sums<-0
+		for(i in 1:k){
+			cshort.i <- cshort[cshort==i]
+			clarge.i <- clarge[clarge==temp[i]]
+			overlap.i <- length(intersect(names(cshort.i),names(clarge.i)))
+			sums <- sums + overlap.i
+		}
+		res.temp<-sums/length(cshort)
+		res<-c(res,res.temp)
+	}
+
+	return(paste0(max(res)*100,"%"))
+}
